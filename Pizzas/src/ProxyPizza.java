609d02ace0824810f927bbd461f0e9a050b1317c
José Luis Quintero C <jquinteroc@acm.org>
2020-02-13 08:27:45 -0500

    Update ProxyPizza.java

--- Pizzas/src/ProxyPizza.java
+++ Pizzas/src/ProxyPizza.java
@@ -1,8 +1,15 @@
 
 public class ProxyPizza implements Pizza {
 
-    @Override
-    public void Comer() {
+	Pizza pizza;
 
+    @Override
+    public void Comer(int seleccion) {
+        if (seleccion == 1){
+            pizza = new PizzaChamp();
+        } else{
+            pizza = new Adaptar();
+        }
+        pizza.comer();
     }
 }
