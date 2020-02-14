759b1fd53ddb6908270cf021cc6e197a710acfa5
JQuinteroC <jlqc1772@gmail.com>
2020-02-13 21:45:33 -0500

    se corrigio el la ejecuación del main, y se adapto un try chatch en la selección de la instancia

--- Pizzas/src/Cliente.java
+++ Pizzas/src/Cliente.java
@@ -1,12 +1,14 @@
+
 import java.util.Scanner;
 
 public class Cliente {
-    
-    Scanner s = new Scanner(System.in);        
 
     public static void main(String[] args) {
-        System.out.println("¿Que pizza desea comer?\n 1=champiñones \n 2=piña");
+        Scanner s = new Scanner(System.in);
         ProxyPizza proxy = new ProxyPizza();
-        proxy.comer(s.nextInt());
+
+        System.out.println("¿Que pizza desea comer?\n 1=champiñones \n 2=piña");
+        proxy.setSeleccion(s.nextInt());
+        proxy.Comer();
     }
 }
--- Pizzas/src/ProxyPizza.java
+++ Pizzas/src/ProxyPizza.java
@@ -1,15 +1,33 @@
 
 public class ProxyPizza implements Pizza {
 
-	Pizza pizza;
+    Pizza pizza;
+    private int seleccion;
 
     @Override
-    public void Comer(int seleccion) {
-        if (seleccion == 1){
-            pizza = new PizzaChamp();
-        } else{
-            pizza = new Adaptar();
+    public void Comer() {
+        try {
+            switch (seleccion) {
+                case 1:
+                    pizza = new PizzaChamp();
+                    break;
+                case 2:
+                    pizza = new Adaptar();
+                    break;
+            }
+            pizza.Comer();
+        } catch (Exception e) {
+            System.out.println("No es opción valida. Proceso finalizado");
+            System.exit(0);
         }
-        pizza.comer();
     }
+
+    public int getSeleccion() {
+        return seleccion;
+    }
+
+    public void setSeleccion(int seleccion) {
+        this.seleccion = seleccion;
+    }
+
 }
